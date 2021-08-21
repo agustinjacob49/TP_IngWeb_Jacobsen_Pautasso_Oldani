e382930ef968df5b028f3ad2171107c75d006ddb
DESKTOP-CUE6MFN\Joaco <joaquinoldani07@gmail.com>
2021-08-21 16:09:08 -0300

    entrega 2 lista, falta estilos en pagina priv

--- organizapp/administrador/url.py
+++ organizapp/administrador/url.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
 from .views import *
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', home, name="home"),
-    path('login/', login, name="login"),
-]
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name="logout"),
+    path('register/', register, name="register"),
+    path('private-page/', login_required(privatePage), name="private_page")
+] 
+#+ statics(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- organizapp/organizapp/settings.py
+++ organizapp/organizapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from django.shortcuts import redirect
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
@@ -123,6 +124,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
