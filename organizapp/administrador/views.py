4a56747b0556da1be80aac5d11b56f879aec91ce
joaquinoldani <59928014+joaquinoldani@users.noreply.github.com>
2021-08-24 10:32:15 -0300

    stylesss

--- organizapp/administrador/views.py
+++ organizapp/administrador/views.py
@@ -27,21 +27,6 @@ def home(request):
     return render(request, 'home.html')
 
 
-def register(request):
-    if request.method=='POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            messages.success(request, f'Usuario {username} ha sido creado')
-            return redirect('home')
-    else:
-        form = UserRegisterForm()
-
-    context = {'form': form}
-    return render(request, 'register.html', context)
-
-
 def register_user(request):
     args = {}
     args.update(csrf(request))
