619839dbd878d0b3d1e6e5bed18942d28b6ebaad
joaquinoldani <59928014+joaquinoldani@users.noreply.github.com>
2021-08-24 16:00:58 -0300

    activation_key with more characters

--- organizapp/administrador/models.py
+++ organizapp/administrador/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime
+from datetime import date
 
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    activation_key = models.CharField(max_length=40, blank=True)
-    key_expires = models.DateTimeField(default=datetime.date.today())
+    activation_key = models.CharField(max_length=255, blank=True)
+    key_expires = models.DateTimeField(default=date.today)
 
     def __str__(self):
         return self.user.username
