618b8ed40504aa2b0a8cd720acd1ecca3b10fa0f
joaquinoldani <59928014+joaquinoldani@users.noreply.github.com>
2021-09-28 16:50:45 -0300

    settings

--- organizapp/organizapp/settings.py
+++ organizapp/organizapp/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'organizapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['organizapp/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
