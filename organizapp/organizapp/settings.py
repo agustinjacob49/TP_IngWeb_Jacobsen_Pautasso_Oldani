090a04a51ef8599fa346d5082fc610d619160dfd
Nicolas Pautasso <nicopautasso95@gmail.com>
2021-10-22 17:11:00 -0300

    fixes haystack

--- organizapp/organizapp/settings.py
+++ organizapp/organizapp/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'administrador'
+    'administrador',
+    'haystack'
 ]
 
 MIDDLEWARE = [
